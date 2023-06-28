055bc177dee2bdb12be35e9a13cdebf3b8793e50
cesardiego10 <ellcdcs26@gmail.com>
2023-06-28 10:13:54 -0600

    primer commit

---
+++ src/main/java/com/mycompany/proyecto_prueba/Proyecto_prueba.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto_prueba;
+
+/**
+ *
+ * @author LENOVO
+ */
+spublic class Proyecto_prueba {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
